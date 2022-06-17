75e4f01d6ebf63f66e000f87f4f377e7bc57b4a6
Tarlan <94639236+tarlan3003@users.noreply.github.com>
2022-06-17 14:56:12 +0400

    Add files via upload

---
+++ GameOfLife/step4/game_sdl/CMakeFiles/game_sdl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_sdl.dir/game_sdl.c.o"
+  "libgame_sdl.a"
+  "libgame_sdl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/game_sdl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
